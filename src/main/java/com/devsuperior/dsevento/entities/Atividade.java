1f26e0f5e41f11b793e2f39267fd92d06001c828
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-02-15 15:59:27 -0300

    Entitity Atividade

--- src/main/java/com/devsuperior/dsevento/entities/Atividade.java
+++ src/main/java/com/devsuperior/dsevento/entities/Atividade.java
@@ -1,34 +1,42 @@
 package com.devsuperior.dsevento.entities;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_categoria")
-public class Categoria {
+@Table(name = "tb_atividade")
+public class Atividade {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String nome;
+    @Column(columnDefinition = "TEXT")
     private String descricao;
+    private Double preco;
 
-    @OneToMany(mappedBy = "categoria")
-    private List<Atividade> atividades = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
 
-    public Categoria() {
+    public Atividade() {
     }
 
-    public Categoria(Integer id, String descricao) {
+    public Atividade(Integer id, String nome, String descricao, Double preco, Categoria categoria) {
         this.id = id;
+        this.nome = nome;
         this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
     }
 
     public Integer getId() {
@@ -37,11 +45,17 @@ public class Categoria {
 
     public void setId(Integer id) {
         this.id = id;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getDescricao() {
-
         return descricao;
     }
 
@@ -49,8 +63,20 @@ public class Categoria {
         this.descricao = descricao;
     }
 
-    public List<Atividade> getAtividades() {
-        return atividades;
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
     }
 
     @Override
@@ -61,7 +87,7 @@ public class Categoria {
         if (o == null || getClass() != o.getClass())
             return false;
 
-        Categoria other = (Categoria) o;
+        Atividade other = (Atividade) o;
         return Objects.equals(id, other.id);
     }
 
